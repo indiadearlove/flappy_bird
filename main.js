9cab6c2cbf14dda87b9516ae06e69e69475363a7
India Dearlove <india_dearlove@yahoo.co.uk>
2015-04-09 12:24:52 +0100

    scoring & finished tutorial

--- main.js
+++ main.js
@@ -25,11 +25,16 @@ var mainState = {
 
     //timer
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    //score
+    this.score = 0;  
+    this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
   },
 
   update: function() {  
     if (this.bird.inWorld == false)
       this.restartGame();
+    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this); 
   },
 
   jump: function() {  
@@ -52,7 +57,9 @@ var mainState = {
     var hole = Math.floor(Math.random() * 5) + 1;
     for (var i = 0; i < 8; i++)
         if (i != hole && i != hole + 1) 
-            this.addOnePipe(400, i * 60 + 10);   
+            this.addOnePipe(400, i * 60 + 10); 
+    this.score += 1;  
+    this.labelScore.text = this.score;   
   },
 
 };
