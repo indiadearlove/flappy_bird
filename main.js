f7d747620b5f1684afe5a1365bc9372f8421c9e7
India Dearlove <india_dearlove@yahoo.co.uk>
2015-04-09 12:07:31 +0100

    adding pipes

--- main.js
+++ main.js
@@ -5,15 +5,23 @@ var mainState = {
   preload: function() {
     game.stage.backgroundColor = '#71c5cf';
     game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
   },
 
   create: function() {  
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    //sprite bird
     this.bird = this.game.add.sprite(100, 245, 'bird');
     game.physics.arcade.enable(this.bird);
     this.bird.body.gravity.y = 1000;  
     var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);     
+    spaceKey.onDown.add(this.jump, this);
+
+    //pipes
+    this.pipes = game.add.group(); 
+    this.pipes.enableBody = true; 
+    this.pipes.createMultiple(20, 'pipe');
   },
 
   update: function() {  
@@ -28,6 +36,14 @@ var mainState = {
   restartGame: function() {  
     game.state.start('main');
   },
+
+  addOnePipe: function(x, y) {  
+    var pipe = this.pipes.getFirstDead();
+    pipe.reset(x, y);
+    pipe.body.velocity.x = -200; 
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+},
 
 };
 
